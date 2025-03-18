632d81ce2f80d2bd198aeb354baa404218c9ced4
freddyolmos <f.olmos@solucionescomsys.com>
2025-03-18 17:46:10 -0600

    FO: Modify max_length in name of Vehicle.

--- odoodjango/vehicles/models/vehicle.py
+++ odoodjango/vehicles/models/vehicle.py
@@ -2,7 +2,7 @@ from django.db import models
 from .brand import Brand
 
 class VehicleModel(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="models")
 
     class Meta:
