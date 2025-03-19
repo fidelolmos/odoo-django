6ecc0e841af99abdb6ffe0f6a55849239c6cb4a8
freddyolmos <f.olmos@solucionescomsys.com>
2025-03-19 14:36:44 -0600

    FO: Create new EndPoint for infractions.

--- odoodjango/infractions/serializers.py
+++ odoodjango/infractions/serializers.py
@@ -1,32 +1,27 @@
 from rest_framework import serializers
-from .models import Infraction
+from infractions.models import Infraction
 
-class InfractionSerializer(serializers.ModelSerializer):
-    # Mapeo de nombres de campos para coincidir con Odoo
-    numeroInfraccion = serializers.IntegerField(source="infraction_number")
-    fechaInfraccion = serializers.DateField(source="date")
-    categoria = serializers.CharField(source="category")
-    conceptoInfraccion = serializers.CharField(source="concept")
-    vehiculo = serializers.CharField(source="vehicle_plate")
-    marca = serializers.CharField(source="brand")
-    subMarca = serializers.CharField(source="model")
-    calle = serializers.CharField(source="street")
-    colonia = serializers.CharField(source="neighborhood")
-    alcaldia = serializers.CharField(source="municipality")
-    pagado = serializers.BooleanField(source="paid")
+class InfractionDetailSerializer(serializers.ModelSerializer):
+    vehicle = serializers.CharField(source="vehicle.plate")
+    brand = serializers.CharField(source="vehicle.model.brand.name")
+    model = serializers.CharField(source="vehicle.model.name")
+    article = serializers.CharField(source="violation_detail.paragraph.subsection.fraction.article.number")
+    fraction = serializers.CharField(source="violation_detail.paragraph.subsection.fraction.number")
+    foundation = serializers.CharField(source="violation_detail.foundation")
+    colony = serializers.CharField(source="location.neighborhood.name")
+    municipality = serializers.CharField(source="location.neighborhood.municipality.name")
 
     class Meta:
         model = Infraction
         fields = [
-            "numeroInfraccion",
-            "fechaInfraccion",
-            "categoria",
-            "conceptoInfraccion",
-            "vehiculo",
-            "marca",
-            "subMarca",
-            "calle",
-            "colonia",
-            "alcaldia",
-            "pagado",
+            "infraction_number",
+            "date",
+            "vehicle",
+            "brand",
+            "model",
+            "article",
+            "fraction",
+            "foundation",
+            "colony",
+            "municipality",
         ]
