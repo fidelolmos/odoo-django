c91d8cb475c4f41aa04a7aa841a3dd8337b2f1b0
freddyolmos <f.olmos@solucionescomsys.com>
2025-03-20 11:39:11 -0600

    FO: Modified infractions/serializers.py .

--- odoodjango/infractions/serializers.py
+++ odoodjango/infractions/serializers.py
@@ -2,14 +2,15 @@ from rest_framework import serializers
 from infractions.models import Infraction
 
 class InfractionDetailSerializer(serializers.ModelSerializer):
-    vehicle = serializers.CharField(source="vehicle.plate")
-    brand = serializers.CharField(source="vehicle.model.brand.name")
-    model = serializers.CharField(source="vehicle.model.name")
-    article = serializers.CharField(source="violation_detail.paragraph.subsection.fraction.article.number")
-    fraction = serializers.CharField(source="violation_detail.paragraph.subsection.fraction.number")
-    foundation = serializers.CharField(source="violation_detail.foundation")
-    colony = serializers.CharField(source="location.neighborhood.name")
-    municipality = serializers.CharField(source="location.neighborhood.municipality.name")
+    vehicle = serializers.CharField(source="vehicle.plate", default="SIN PLACA")
+    brand = serializers.CharField(source="vehicle.model.brand.name", default="DESCONOCIDO")
+    model = serializers.CharField(source="vehicle.model.name", default="DESCONOCIDO")
+    article = serializers.SerializerMethodField()
+    fraction = serializers.SerializerMethodField()
+    foundation = serializers.SerializerMethodField()
+    colony = serializers.CharField(source="location.neighborhood.name", default="SIN COLONIA")
+    municipality = serializers.CharField(source="location.neighborhood.municipality.name", default="SIN MUNICIPIO")
+    paid = serializers.BooleanField()
 
     class Meta:
         model = Infraction
@@ -24,4 +25,21 @@ class InfractionDetailSerializer(serializers.ModelSerializer):
             "foundation",
             "colony",
             "municipality",
+            "paid",
         ]
+
+    def get_article(self, obj):
+        """ Devuelve el artículo o 'DESCONOCIDO' si violation_detail es NULL """
+        if obj.violation_detail and obj.violation_detail.paragraph:
+            return getattr(obj.violation_detail.paragraph.subsection.fraction.article, "number", "DESCONOCIDO")
+        return "DESCONOCIDO"
+
+    def get_fraction(self, obj):
+        """ Devuelve la fracción o 'DESCONOCIDO' si violation_detail es NULL """
+        if obj.violation_detail and obj.violation_detail.paragraph:
+            return getattr(obj.violation_detail.paragraph.subsection.fraction, "number", "DESCONOCIDO")
+        return "DESCONOCIDO"
+
+    def get_foundation(self, obj):
+        """ Devuelve la fundamentación o 'SIN FUNDAMENTACIÓN' si violation_detail es NULL """
+        return obj.violation_detail.foundation if obj.violation_detail else "SIN FUNDAMENTACIÓN"
