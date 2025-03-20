210e1a7ae1ce9fc25ebc1865015011e874d40574
freddyolmos <f.olmos@solucionescomsys.com>
2025-03-20 10:54:49 -0600

    FO: Changes in settings.py.

--- odoodjango/odoodjango/settings.py
+++ odoodjango/odoodjango/settings.py
@@ -130,11 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles/'
 
+# Media files (para archivos subidos por usuarios)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
